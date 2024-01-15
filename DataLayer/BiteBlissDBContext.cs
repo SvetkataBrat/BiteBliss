f2dc2011e56d479672de0b22e934b8331f193e80
monski2005 <monski2005@abv.bg>
2024-01-15 18:59:06 +0200

    Adding Stage 1 of DataLayer

---
+++ DataLayer/BiteBlissDBContext.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class BiteBlissDBContext : DbContext
+    {
+
+        public BiteBlissDBContext() : base() { }
+
+        public BiteBlissDBContext(DbContextOptions options) : base(options) { }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=TIMI-PC;Database=BiteBliss;Trusted_Connection=True;Encrypt=False");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Recipe> Recipies { get; set; }
+
+    }
+}
